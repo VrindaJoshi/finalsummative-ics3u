6d9f19576d456a4a9a6f8454c75745319f48a4a8
vrindaj <vrinda.joshi@hotmail.com>
2024-01-18 00:01:55 -0500

    more work on activity, finished kitchen, need to work on return to board

--- ClassesObjectsCAI/src/ActivityClass.java
+++ ClassesObjectsCAI/src/ActivityClass.java
@@ -1,12 +1,16 @@
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 import javax.swing.*;
 
-public class ActivityClass extends JFrame {
+public class ActivityClass extends JFrame implements ActionListener {
 
 	// colors
 	static final Color PINK = Color.decode("#FFE1E1");
@@ -25,12 +29,17 @@ public class ActivityClass extends JFrame {
 	private static JLabel constLabel = new JLabel(
 			"<html>here's the recipe for a basic cake! modify <br>it to make the perfect cake for your customer!!</html>");
 	private static JLabel cakeConstructor = new JLabel(new ImageIcon("images/cake.png"));
-	
-	private static JLabel order = new JLabel(new ImageIcon(new ImageIcon("images/orders/order"+currentCake+".png").getImage().getScaledInstance(300, 300, java.awt.Image.SCALE_SMOOTH)));
 
-
+	private static JLabel order = new JLabel(new ImageIcon(new ImageIcon("images/orders/order" + currentCake + ".png")
+			.getImage().getScaledInstance(300, 300, java.awt.Image.SCALE_SMOOTH)));
 
 	private static JLabel welcomeLabel = new JLabel("welcome to the kitchen!");
+
+	private static JLabel cakeInstructions = new JLabel(
+			"<html>create the cake in the box below! name the cake <i>cake" + currentCake + "<i>!</html>");
+	private static JTextField answerField = new JTextField();
+
+	private static JButton nextStep = new JButton("serve your cake!");
 
 	// constructor method to set up frame
 	public ActivityClass() {
@@ -80,31 +89,31 @@ public class ActivityClass extends JFrame {
 		addKeyListener(board);
 
 	}
-	
+
 	// https://docs.oracle.com/javase/tutorial/uiswing/examples/components/TabbedPaneDemoProject/src/components/TabbedPaneDemo.java
 	protected static JComponent makeTextPanel(String text) {
-        JPanel panel = new JPanel(false);
-        panel.setLayout(null);
-        panel.setBackground(Color.white);
+		JPanel panel = new JPanel(false);
+		panel.setLayout(null);
+		panel.setBackground(Color.white);
 
-        return panel;
-    } 
-	
-	 /** Returns an ImageIcon, or null if the path was invalid. */
-    protected static ImageIcon createImageIcon(String path) {
-        java.net.URL imgURL = ActivityClass.class.getResource(path);
-        if (imgURL != null) {
-            return new ImageIcon(imgURL);
-        } else {
-            System.err.println("Couldn't find file: " + path);
-            return null;
-        }
-    }
-	//-------------------------------------------------------
+		return panel;
+	}
+
+	/** Returns an ImageIcon, or null if the path was invalid. */
+	protected static ImageIcon createImageIcon(String path) {
+		java.net.URL imgURL = ActivityClass.class.getResource(path);
+		if (imgURL != null) {
+			return new ImageIcon(imgURL);
+		} else {
+			System.err.println("Couldn't find file: " + path);
+			return null;
+		}
+	}
+	// -------------------------------------------------------
 
 	static void setCustomer() {
-		
-		//basic for all orders----------
+
+		// basic for all orders----------
 
 		mainPanel.setBackground(PINK);
 		mainPanel.setBounds(0, 0, 1366, 766);
@@ -115,10 +124,9 @@ public class ActivityClass extends JFrame {
 		topPanel.setBackground(Color.WHITE);
 		topPanel.setLayout(null);
 		mainPanel.add(topPanel);
-		
+
 		JTabbedPane tabbedPane = new JTabbedPane();
 		ImageIcon icon = createImageIcon("cakeIcon.png");
-
 
 		JComponent panel1 = makeTextPanel("Panel #1");
 		//
@@ -128,21 +136,19 @@ public class ActivityClass extends JFrame {
 
 		cakeConstructor.setBounds(20, 40, 445, 235);
 		panel1.add(cakeConstructor);
-		
-		tabbedPane.addTab("Basic Cake Recipe", icon, panel1,
-		                  "Does nothing");
+
+		tabbedPane.addTab("Basic Cake Recipe", icon, panel1, null);
 		tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
 
 		JComponent panel2 = makeTextPanel("Panel #2");
-		
+
 		order.setBounds(0, 0, 500, 300);
 		panel2.add(order);
-		
-		tabbedPane.addTab("Customer's Order", icon, panel2,
-		                  "Does twice as much nothing");
+
+		tabbedPane.addTab("Customer's Order", icon, panel2, null);
 		tabbedPane.setMnemonicAt(1, KeyEvent.VK_2);
-		
-		tabbedPane.setBounds(50, 150, 500, 300);
+
+		tabbedPane.setBounds(50, 150, 500, 350);
 		topPanel.add(tabbedPane);
 
 		// set title
@@ -150,7 +156,46 @@ public class ActivityClass extends JFrame {
 		welcomeLabel.setFont(MainMenuClass.italiana.deriveFont(60f));
 		topPanel.add(welcomeLabel);
 
-		//------------
+		// ------------
+
+		cakeInstructions.setBounds(600, 250, 400, 30);
+		cakeInstructions.setFont(MainMenuClass.inter.deriveFont(15f));
+		topPanel.add(cakeInstructions);
+
+		answerField.setBounds(600, 290, 450, 60);
+		answerField.setBorder(BorderFactory.createLineBorder(Color.black));
+		answerField.setFont(MainMenuClass.inter.deriveFont(15f));
+		topPanel.add(answerField);
+
+		// button
+		nextStep.setBounds(730, 370, 200, 50);
+		nextStep.setFont(MainMenuClass.inter.deriveFont(15f));
+		nextStep.setBackground(PINK);
+		nextStep.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				// set up blank panel
+				mainPanel.setBounds(50, 50, 1166, 616);
+				mainPanel.add(board);
+
+				// key listener for board so pacman can move
+				//addKeyListener(board);
+				
+				mainPanel.remove(topPanel);
+				
+				mainPanel.revalidate();
+				mainPanel.repaint();
+				
+			}
+		});
+		topPanel.add(nextStep);
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
 
 	}
 
